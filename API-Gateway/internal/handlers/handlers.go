317bc7f86121b1743c814b0547890c2068c306c2
Alexander Taran <taransasha004@gmail.com>
2025-10-26 23:32:49 +0300

    коррекция методов

--- API-Gateway/internal/handlers/handlers.go
+++ API-Gateway/internal/handlers/handlers.go
@@ -48,25 +48,26 @@ func (h *Handler) initRoutes() {
 	// === User routes ===
 	users := api.Group("/users")
 	users.Get("/", h.GetUsers, RoleMiddleware(ROLE_DEVELOPER, ROLE_STUDENT, ROLE_HR))
-
-	profileUser := users.Group("/:id")
-	profileUser.Get("/", h.GetUser, RoleMiddleware(ROLE_DEVELOPER, ROLE_STUDENT, ROLE_HR))
-	profileUser.Patch("/edit", h.UpdateUser, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_STUDENT))
-	profileUser.Delete("/", h.DeleteUser, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_STUDENT))
+	users.Get("/me", h.GetMe, RoleMiddleware(ROLE_DEVELOPER, ROLE_STUDENT))
+	users.Get("/:id", h.GetUser, RoleMiddleware(ROLE_DEVELOPER, ROLE_STUDENT, ROLE_HR))
+	users.Patch("/edit", h.UpdateUser, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_STUDENT))
+	users.Delete("/", h.DeleteUser, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_STUDENT))
 
 	// === User Achievement routes ===
-	userAchievement := profileUser.Group("/achievement")
+	userAchievement := users.Group("/achievement")
 	userAchievement.Get("/", h.GetUserAchievements, RoleMiddleware(ROLE_DEVELOPER, ROLE_STUDENT, ROLE_HR))
 	userAchievement.Get("/id", h.CreateUserAchievement, RoleMiddleware(ROLE_DEVELOPER, ROLE_STUDENT, ROLE_HR))
 	userAchievement.Post("/", h.CreateUserAchievement, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_STUDENT))
 	userAchievement.Delete("/id", h.DeleteAchievement, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_STUDENT))
 
 	// === HR routes ===
-	profileHR := api.Group("/hr/:id")
+	profileHR := api.Group("/hr")
+	profileHR.Get("/me", h.GetMe, RoleMiddleware(ROLE_DEVELOPER, ROLE_HR))
+	profileHR.Patch("/edit", h.UpdateUser, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_HR))
 	profileHR.Delete("/", h.DeleteUser, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_HR))
 
 	// === HR vacancy routes ===
-	HRVacancy := api.Group("/vacancy")
+	HRVacancy := profileHR.Group("/vacancy")
 	HRVacancy.Get("/", h.GetHRVacancies, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_HR))
 	HRVacancy.Get("/:id", h.GetVacancy, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_HR))
 	HRVacancy.Post("/", h.CreateHRVacancy, OwnerOrRoleMiddleware(ID, ROLE_DEVELOPER, ROLE_HR))
--- API-Gateway/internal/handlers/user.go
+++ API-Gateway/internal/handlers/user.go
@@ -67,6 +67,51 @@ func (h *Handler) GetUsers(c *fiber.Ctx) error {
 	return c.JSON(profileList)
 }
 
+func (h *Handler) GetMe(c *fiber.Ctx) error {
+	log.Printf("GetUsers: Getting me")
+	userID := getUserIDFromContext(c)
+
+	// Валидация UUID
+	if _, err := uuid.Parse(userID); err != nil {
+		log.Printf("GetUser: Invalid UUID format: %s", userID)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid user ID format",
+		})
+	}
+
+	// Вызываем users service
+	profile, err := h.apiService.User.GetUser(c.Context(), userID)
+	if err != nil {
+		log.Printf("GetUser: Failed to get user %s: %v", userID, err)
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "User not found",
+		})
+	}
+
+	// Конвертируем в HTTP модель
+	user := models.User{
+		ID:                 uuid.MustParse(profile.Id),
+		FirstName:          profile.FirstName,
+		LastName:           profile.LastName,
+		Age:                profile.Age,
+		Tg:                 profile.Tg,
+		Email:              profile.Email,
+		Description:        profile.Description,
+		ProfessionCategory: profile.ProfessionCategory,
+	}
+
+	// Обрабатываем resume_id
+	if profile.ResumeId != "" {
+		user.ResumeID = uuid.MustParse(profile.ResumeId)
+	}
+
+	// TODO: вытащить ссылку на резюме
+
+	log.Printf("GetUser: Successfully retrieved user: %s", userID)
+	return c.JSON(user)
+
+}
+
 // GetUser возвращает профиль пользователя по ID
 func (h *Handler) GetUser(c *fiber.Ctx) error {
 	userID := c.Params("id")
@@ -114,17 +159,8 @@ func (h *Handler) GetUser(c *fiber.Ctx) error {
 
 // UpdateUser обновляет профиль пользователя (PATCH)
 func (h *Handler) UpdateUser(c *fiber.Ctx) error {
-	userID := c.Params("id")
-	currentUserID := getUserIDFromContext(c)
-	log.Printf("UpdateUser: Updating user %s by user %s", userID, currentUserID)
-
-	// Проверяем что пользователь обновляет свой профиль
-	if userID != currentUserID {
-		log.Printf("UpdateUser: User %s trying to update profile of user %s", currentUserID, userID)
-		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
-			"error": "Can only update your own profile",
-		})
-	}
+	userID := getUserIDFromContext(c)
+	log.Printf("UpdateUser: Updating user %s", userID)
 
 	// Валидация UUID
 	if _, err := uuid.Parse(userID); err != nil {
@@ -206,10 +242,9 @@ func (h *Handler) UpdateUser(c *fiber.Ctx) error {
 
 // DeleteUser удаляет профиль пользователя
 func (h *Handler) DeleteUser(c *fiber.Ctx) error {
-	userID := c.Params("id")
-	currentUserID := getUserIDFromContext(c)
+	userID := getUserIDFromContext(c)
 	currentUserRole := getRoleFromContext(c)
-	log.Printf("DeleteUser: Deleting user %s by user %s (role: %s)", userID, currentUserID, currentUserRole)
+	log.Printf("DeleteUser: Deleting user %s (role: %s)", userID, currentUserRole)
 
 	// Валидация UUID
 	if _, err := uuid.Parse(userID); err != nil {
